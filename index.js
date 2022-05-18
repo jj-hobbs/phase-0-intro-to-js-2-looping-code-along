966c3b82e7c995ed52644460715f17b7ffe25abd
justin.hobbs <justinhobbs@fastmail.com>
2022-05-18 08:18:25 -0500

    commit tree 53c7bfa81870f03de14b6184d1eadf8b6e93d8f9

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
-// Code your solutions in this file
+function countDown() {
+    let countDown = 10;
+while (countDown >= 0) {
+  console.log(countDown--);
+}
+}
+
+// const cards = [[], "birthday"];
+
+function writeCards(cards, type) {
+    const messages = [];
+    for (let i = 0; i < cards.length; i++) {
+      console.log(`Thank you, ${cards[i]}, for the wonderful ${type} gift!`);
+      messages.push(`Thank you, ${cards[i]}, for the wonderful ${type} gift!`)
+    }
+  
+    return messages;
+  }
+  
+//   writeCards(cards);
